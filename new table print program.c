3c1a95504d9bb8762572d79dbb04285862fe4849
Jyoti R Sulakhe <122368666+Jyotisulakhe@users.noreply.github.com>
2023-01-28 11:02:31 +0530

    Add files via upload

---
+++ new table print program.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+//new type of program to print table
+int main()
+{
+    int i,table;
+  for( i=1; i<=10; i++){
+    table=23*i;   //(just type  number here which table you see )
+
+    printf("%d\n",table);
+  }
+    return 0;
+}
